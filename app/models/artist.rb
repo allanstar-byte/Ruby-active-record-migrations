e168b24ce959acebb43784d1848b846f99a8c338
allanstar-byte <allanmutisya263@gmail.com>
2022-09-21 09:59:11 +0300

    Migrations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,23 @@
 # add the Artist class here
+require 'active_record'
+
+# Coneccting to a database
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/artists.sqlite"
+)
+
+#  SQL to create the table:
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+    id INTEGER PRIMARY KEY,
+    Name TEXT,
+    Genre TEXT,
+    Age INTEGER,
+    Hometown TEXT
+    )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
